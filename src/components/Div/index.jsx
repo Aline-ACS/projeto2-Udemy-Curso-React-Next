57d2ccb3aaf10435ab64a37d209da3e5e13aa01a
Aline-ACS <aline_capoani@hotmail.com>
2021-06-02 09:09:02 -0300

    UseContext

---
+++ src/components/Div/index.jsx
@@ -0,0 +1,12 @@
+import { H1 } from '../H1/index';
+import { Paragrafo } from '../Paragrafo/index';
+
+// eslint-disable-next-line
+export const Div = ({ children }) => {
+  return (
+    <>
+      <H1 />
+      <Paragrafo />
+    </>
+  );
+};
---
+++ src/contexts/AppContext/data.js
@@ -0,0 +1,5 @@
+export const globalState = {
+  title: 'titulo do contexto',
+  body: 'body do contexto',
+  counter: 0,
+};
